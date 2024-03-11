f88659dda7ece5c5cbaf953a61a19c066aebb467
Kimsanlim <kimsanlim27@gmail.com>
2024-03-12 01:09:54 +0700

    Add bootstrap, ejs, and create route

--- src/app.js
+++ src/app.js
@@ -3,10 +3,15 @@ const bodyParser = require('body-parser');
 
 const sequelize = require('./utils/database');
 const User = require('./models/user');
+const routes = require('./routes/route');
 
 const app = express();2
 
+app.set('view engine', 'ejs');  // Using ejs as the view engine
+app.set('views', 'src/views');   // implies that Express should look for view files inside a directory named "views"
+
 app.use(bodyParser.json());
+app.use(routes);
 
 sequelize
   .sync()
