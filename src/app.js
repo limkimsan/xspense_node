a6eb57d8a287c3b646dc140cba86689ac7993fde
Kimsanlim <kimsanlim27@gmail.com>
2024-03-12 15:04:41 +0700

    Create login page UI

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
+const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
 const sequelize = require('./utils/database');
 const User = require('./models/user');
 const routes = require('./routes/route');
+const authRoutes = require('./routes/auth_route');
 
 const app = express();2
 
@@ -11,7 +13,9 @@ app.set('view engine', 'ejs');  // Using ejs as the view engine
 app.set('views', 'src/views');   // implies that Express should look for view files inside a directory named "views"
 
 app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'assets')));
 app.use(routes);
+app.use(authRoutes);
 
 sequelize
   .sync()
