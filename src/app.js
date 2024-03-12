ce43d0d81815a2ad440bf999015c728c89c00310
Kimsanlim <kimsanlim27@gmail.com>
2024-03-12 17:16:48 +0700

    Integrate express-mysql-session

--- src/app.js
+++ src/app.js
@@ -2,13 +2,22 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const session = require('express-session');
+const MySQLStore = require('express-mysql-session')(session);
 
 const sequelize = require('./utils/database');
 const User = require('./models/user');
 const routes = require('./routes/route');
 const authRoutes = require('./routes/auth_route');
 
-const app = express();2
+const app = express();
+
+const sessionStore = new MySQLStore({
+  host: 'localhost',
+	port: 3306,
+	user: 'root',
+	password: 'MySql@2024',
+	database: 'xspense-track'
+});
 
 app.set('view engine', 'ejs');  // Using ejs as the view engine
 app.set('views', 'src/views');   // implies that Express should look for view files inside a directory named "views"
@@ -19,7 +28,8 @@ app.use(express.static(path.join(__dirname, 'assets')));
 app.use(session({
   secret: 'my secret',
   resave: false,
-  saveUninitialized: false
+  saveUninitialized: false,
+  store: sessionStore
 }));
 
 app.use(routes);
