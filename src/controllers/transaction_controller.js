e7dcd7c47f16d425016c8bbf4ff4fcd5632d272d
Kimsanlim <kimsanlim27@gmail.com>
2024-03-22 17:07:57 +0700

    Add max date for date picker

--- src/controllers/transaction_controller.js
+++ src/controllers/transaction_controller.js
@@ -60,6 +60,11 @@ exports.getCreateTransaction = (req, res, next) => {
       return incomeCates;
     })
     .then(incomeCates => {
+      const date = new Date();
+      const year = date.getFullYear();
+      const month = String(date.getMonth() + 1).padStart(2, '0'); // Add leading zero for single-digit months
+      const day = String(date.getDate()).padStart(2, '0');
+
       Category.findAll({ raw: true, where: { transaction_type: 1 } })
         .then(expenseCates => {
           res.render('transactions/new', {
@@ -67,6 +72,7 @@ exports.getCreateTransaction = (req, res, next) => {
             incomeCates: incomeCates,
             expenseCates: expenseCates,
             isEdit: false,
+            maxDate: `${year}-${month}-${day}`,
             oldInput: {
               amount: '',
               currency_type: null,
